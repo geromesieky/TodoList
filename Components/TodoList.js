d211428524e5db79e6338da31922dd0403f27cd3
sieky <geromesiekytounkara@gmail.com>
2023-10-18 09:33:37 +0000

    React native fundamentals : Components, State, Props

---
+++ Components/TodoList.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import Todo from './Todo';
+
+const TodoList = () => {
+
+    const [title, setTitle ] = useState('TodoList')
+    return(
+        <View>
+            <Text>{title}</Text>
+            <Todo name='First Todo'/>
+            <Todo name='Second Todo'/>
+            <Button title='Change me' onPress={() => setTitle('My List')} />
+        </View>
+    )
+}
+
+export default TodoList;
